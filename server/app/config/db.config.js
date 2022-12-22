e601befd94f54b38266248e0a3ce44b02bddeafd
pravin.devakar <pravin.devakar@globallogic.com>
2022-12-22 10:20:06 +0530

    client side added

--- server/app/config/db.config.js
+++ server/app/config/db.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  url: "mongodb://127.0.0.1:27017/SocialMediaProject",
+  // url: "mongodb://127.0.0.1:27017/SocialMediaProject",
+  url: "mongodb+srv://Pravindevkar007:vJkBiwoSobKVbvAK@cluster0.xmbkede.mongodb.net/?retryWrites=true&w=majority",
 };
+
+
+
--- server/server.js
+++ server/server.js
@@ -9,9 +9,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 const db = require("./app/models");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 db.mongoose
-  .connect(db.url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .connect(db.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+  })
   .then(() => {
     console.log("connected to Database successfully !!!");
   })
@@ -19,6 +24,25 @@ db.mongoose
     console.log("Failed to connect database !!!");
     process.exit();
   });
+
+  // const { MongoClient, ServerApiVersion } = require("mongodb");
+  // const uri =
+  //   "mongodb+srv://Pravindevkar007:vJkBiwoSobKVbvAK@cluster0.xmbkede.mongodb.net/?retryWrites=true&w=majority";
+  // const client = new MongoClient(uri, {
+  //   useNewUrlParser: true,
+  //   useUnifiedTopology: true,
+  //   serverApi: ServerApiVersion.v1,
+  // });
+  // client.connect((err) => {
+  //   const collection = client.db("test").collection("devices");
+  //   // perform actions on the collection object
+  //   console.log(err);
+  //   client.close();
+  // });
+
+
+
+
 
 require("./app/routes/socialpost.routes")(app);
 require("./app/routes/users.routes")(app);
